e6c8c83f02b9d4e3c13d81036cce1d45682ad6f6
Rodney <pokerdiy@gmail.com>
2018-02-14 11:05:02 +1100

    Implemented API methods
    First draft

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
